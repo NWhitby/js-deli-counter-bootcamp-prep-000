05616218b2f2da4b022d76b6e1e5c0f2b8c5a5fa
NaKia Whitby <whitby.nakia@gmail.com>
2018-05-26 22:42:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,17 +4,3 @@ function takeANumber(katzDeliLine, name) {
 }
 
 
-function currentLine(katzDeli){
-  if (katzDeli.length === 0){
-    return `The line is currently empty.`;
-  }
-  else {
-  let info = [];
-  for(var i = 0; i < katzDeli.length; i += 1){
-    info.push(parseInt([i])+1 +'. '+katzDeli[i]);
-  }
-  
-  return `The line is currently empty`;
-}}
-
-currentLine(["Ada", "Grace"]);
