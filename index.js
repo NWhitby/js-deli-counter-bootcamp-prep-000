4a88a1fdba52abb8b2a5bc5a12f2f63f33c9af13
NaKia Whitby <whitby.nakia@gmail.com>
2018-05-26 22:45:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
+function nowServing(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    i += 1;
+  }
+  if (katzDeli.length === 0) {
+    return `There is nobody waiting to be served!`;
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
+}
 
