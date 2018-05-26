7547ebe57e6410a7e1ab748360dd96af7986c15d
NaKia Whitby <whitby.nakia@gmail.com>
2018-05-26 22:33:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function currentLine(katzDeli){
-  let info = []; 
-    for(let i = 0; i <katzDeli.length; i +=){
-    info.push(parseInt([i]) +1+'. '+katzDeli[i]);
-  }
-  
-  return "The line is currently: "+ results.join(', ')
-}}
+function nowServing (katzDeliLine) { 
+  if(katzDeliLine.length === 0) { //checks if length of array(katzDeli) is = to 0
+//    return 'There is nobody waiting to be served!'; //returns string no one is waiting
+// }
+//    var customer = katzDeliLine.shift(); // shift array of people
+//    return "Currently serving " + customer + "."; //return string with customer name
+// }
