b87396703828c97be25ed3d971f241a817a77fee
NaKia Whitby <whitby.nakia@gmail.com>
2018-05-26 22:40:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ function takeANumber(katzDeliLine, name) {
 
 
 function currentLine(katzDeli){
-  if (katzDeli.length===0){
-    return "The line is currently empty.";
+  if (katzDeli.length === 0){
+    return `The line is currently empty.`;
   }
   else {
-  var results = [];
-  for(var i=0; i<katzDeli.length; i++){
-    results. push(parseInt([i])+1+'. '+katzDeli[i]);
+  let info = [];
+  for(var i = 0; i < katzDeli.length; i += 1){
+    info.push(parseInt([i])+1 +'. '+katzDeli[i]);
   }
   
-  return "The line is currently: "+ results.join(', ')
+  return `The line is currently ${info}`;
 }}
