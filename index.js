0aff825cbb2b5fb23c2901203fe25984c17eb17f
NaKia Whitby <whitby.nakia@gmail.com>
2018-05-26 23:00:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,13 +25,9 @@ function currentLine(katzDeli) {
 
 
 function currentLine(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
+
+ for (let i = 0; i < katzDeli.length; i++) {
+ line.push(` `+[i+1]+`. `  + katzDeli[i])
 }
+ return(`The line is currently:` + line);
+ }
